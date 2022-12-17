8ab1d12202b3f80161ce0de8f706c50154d72083
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-12-16 23:54:22 -0500

    profile logo

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ export const Header = () => {
   <header onClick={()=>navigate('/')}>
       <img id='overlap_logo' src='/overlap_logo.png'></img>
       <h1>OVERLAP</h1>
-      <div id='profile_logo'></div>      
+      <img id='profile_logo' src='/profile_logo.png'></img>      
   </header>
   )
 }
